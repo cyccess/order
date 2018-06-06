50af810a37da891109876c8c51b713fcf0441501
cyccess <cyccess@gmail.com>
2018-06-06 11:28:14 +0800

    销售统计

---
+++ Cyc.Order.Web/Models/ShopSaleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cyc.Order.Web.Models
+{
+    public class ShopSaleViewModel
+    {
+        public string ShopName { get; set; }
+
+        public int SaleCount { get; set; }
+
+        public decimal TotalAmount { get; set; }
+    }
+}
